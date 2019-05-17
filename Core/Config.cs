48712d6920a111830379dbf0e145a9a476bfa59d
DWVoid <yshliu0321@icloud.com>
2019-05-18 00:25:03 +0800

    Add Config

---
+++ Core/Config.cs
@@ -0,0 +1,19 @@
+﻿using Config.Net;
+
+namespace Akarin
+{
+    public static class Config
+    {
+        private static string Path;
+        
+        public static void Init(string path)
+        {
+            Path = System.IO.Path.GetFullPath(path);
+        }
+
+        public static T GetPool<T>(string pool) where T: class
+        {
+            return new ConfigurationBuilder<T>().UseJsonConfig($"{Path}/{pool}.json").Build();
+        }
+    }
+}
